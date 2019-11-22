e32b5de14ac3461e76da7d66d862c1d1dfb3530c
Matan Kushner <hello@matchai.me>
2019-11-22 14:55:41 -0500

    Get it working!

--- src/code.ts
+++ src/code.ts
@@ -1,31 +1,93 @@
+const chroma = require("chroma-js");
+const bezierEasing = require("bezier-easing");
+
+interface Color {
+  color: { r: number, g: number, b: number, a: number };
+  position: number;
+}
+
+const easeMap = {
+  ease: [0.25, 0.1, 0.25, 1],
+  "ease-in": [0.42, 0, 1, 1],
+  "ease-out": [0, 0, 0.58, 1],
+  "ease-in-out": [0.42, 0, 0.58, 1]
+};
+
 function main(): void {
   const selection = figma.currentPage.selection;
-
-  const fillWithGradient = getFillWithGradient(selection);
-  if (fillWithGradient === null) {
+  // console.log(selection)
+  if (selection.length !== 1 || !isShape(selection[0])) {
     figma.notify("Please select a shape with a gradient");
     figma.closePlugin();
   }
-  
-  console.log(fillWithGradient);
-  
+
+  let node = <GeometryMixin>selection[0];
+  const fillIndex = getFillIndexWithGradient(node);
+  if (fillIndex === null) {
+    figma.notify("Please select a shape with a gradient");
+    figma.closePlugin();
+  }
+
+  const fill = node.fills[fillIndex];
+  if (fill.gradientStops.length !== 2) {
+    figma.notify("Please ensure that the shape's gradient has two colors");
+    figma.closePlugin();
+  }
+
+  // console.log(fill);
+
+  const newColors = easeGradient(fill, 15);
+  // console.log(newColors);
+
+  applyColors(node, fillIndex, newColors);
+
   figma.closePlugin();
 }
 
 main();
 
-function getFillWithGradient(selection: readonly SceneNode[]): GradientPaint | null {
-  if (selection.length !== 1) return null;
-  
-  const node = selection[0];
-  if (!isShape(node)) return null;
+function applyColors(node: GeometryMixin, fillIndex: number, colors: Color[]) {
+  const tempFill = clone(node.fills);
+  tempFill[fillIndex].gradientStops = colors;
+  node.fills = tempFill
+}
 
+function clone(val) {
+  return JSON.parse(JSON.stringify(val));
+}
+
+function easeGradient(fill: GradientPaint, colorSteps: number): Color[] {
+  const colors = fill.gradientStops.map(stop => stop.color);
+  const scale = chroma.scale(colors);
+
+  const bezierCurve = easeMap.ease;
+  const easing = bezierEasing(...bezierCurve);
+
+  const stepSize = 1 / colorSteps;
+  const curvePositions = [];
+  for (let i = 0; i <= colorSteps; i++) {
+    curvePositions.push(i * stepSize);
+  }
+
+  // console.log(curvePositions)
+  return curvePositions.map(position => {
+    const [r, g, b, a] = scale(easing(position)).rgba();
+    return {
+      color: { r, g, b, a },
+      position
+    };
+  });
+}
+
+function getFillIndexWithGradient(node: GeometryMixin): number | null {
+  if (!isShape(node)) return null;
+  
   // We can be assured that node.fills is not a symbol, since it is only a symbol
   // when it is "mixed", which is not possible when only one node is selected.
   const nodeFills = node.fills as readonly Paint[];
-  const gradientFill = nodeFills.find(isGradient);
+  const fillIndex = nodeFills.findIndex(isGradient);
 
-  return gradientFill || null;
+  return fillIndex !== undefined ? fillIndex : null;
 }
 
 function isShape(node: any): node is GeometryMixin {
@@ -35,40 +97,3 @@ function isShape(node: any): node is GeometryMixin {
 function isGradient(paint: any): paint is GradientPaint {
   return (paint as GradientPaint).gradientStops !== undefined;
 }
-
-// function isShapeWithGradient(selection: SceneNode[]) {
-//   return false;
-// }
-
-// figma.showUI(__html__)
-
-// figma.ui.onmessage = msg => {
-//   if (msg.type === 'gradientify') {
-//     const selection = figma.currentPage.selection;
-
-
-
-//     console.log(figma.currentPage.selection);
-    
-    // const selection = figma.currentPage.selection;
-    // console.log(selection)
-
-  //   if (selection instanceof )
-
-  //   const nodes = []
-
-  //   for (let i = 0; i < msg.count; i++) {
-  //     const rect = figma.createRectangle()
-  //     rect.x = i * 150
-  //     rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}]
-  //     figma.currentPage.appendChild(rect)
-  //     nodes.push(rect)
-  //   }
-
-  //   figma.currentPage.selection = nodes
-  //   figma.viewport.scrollAndZoomIntoView(nodes)
-  // }
-
-  // figma.closePlugin()
-  // }
-// }
